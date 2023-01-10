308d6201900b9135a201fb2358c191923da8e152
LxRuii1 <121108358+LxRuii1@users.noreply.github.com>
2023-01-10 08:35:30 +0800

    Update serve.js

--- serve.js
+++ serve.js
@@ -10,8 +10,8 @@ setTimeout(() => {
 
 // 输入账号密码
 module.exports.obj = {
-  phone: '15170764528',
-  password: 'Liao2041936638',
+  phone: '15579742513',
+  password: 'Hf123456',
   title: text.dayTitle[numX],
   content: text.dayContent[numX],
 }
